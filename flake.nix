863d17bbacb0c457450205ddacfa66ac2ede4b69
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-01-16 03:00:02 +0300

    build(nix): use mkDevShell, fix wrong inputs follows usage

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   description = "Flake for lockdown";
 
-  inputs = rec {
+  inputs = {
+    devshell.url = "github:numtide/devshell";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     naersk = {
       url = "github:nmattia/naersk";
-      inputs.nixpkgs = nixpkgs;
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     flakeUtils.url = "github:numtide/flake-utils";
     nixpkgsMoz = {
@@ -19,21 +20,21 @@
     eachSystem defaultSystems (system:
       let
         common = import ./nix/common.nix {
-          sources = { inherit naersk nixpkgs nixpkgsMoz; };
+          sources = { inherit devshell naersk nixpkgs nixpkgsMoz; };
           inherit system;
         };
 
         packages = {
           # Compiles slower but has tests and faster executable
-          "lockdown" = import ./nix/build.nix {
+          lockdown = import ./nix/build.nix {
             inherit common;
             doCheck = true;
             release = true;
           };
           # Compiles faster but no tests and slower executable
-          "lockdown-debug" = import ./nix/build.nix { inherit common; };
+          lockdown-debug = import ./nix/build.nix { inherit common; };
           # Compiles faster but has tests and slower executable
-          "lockdown-tests" = import ./nix/build.nix { inherit common; doCheck = true; };
+          lockdown-tests = import ./nix/build.nix { inherit common; doCheck = true; };
         };
 
       in
@@ -41,7 +42,7 @@
         inherit packages;
 
         # Release build is the default package
-        defaultPackage = packages."lockdown";
+        defaultPackage = packages.lockdown;
 
         devShell = import ./nix/devShell.nix { inherit common; };
       }
